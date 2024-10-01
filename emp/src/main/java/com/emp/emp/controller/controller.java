0fed34a9f44ab6c96731947eefce46c4199f1402
Sujay Zope <sujayazope@gmail.com>
2024-10-01 19:11:19 +0530

    update

--- emp/src/main/java/com/emp/emp/controller/controller.java
+++ emp/src/main/java/com/emp/emp/controller/controller.java
@@ -1,68 +1,116 @@
 package com.emp.emp.controller;
 
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.util.Map;
+import java.util.List;
 
-import org.springframework.core.io.InputStreamResource;
-import org.springframework.core.io.Resource;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.emp.emp.helper.Helper;
+import com.emp.emp.entity.employe;
+import com.emp.emp.service.EmailService;
 import com.emp.emp.service.EmpService;
 
-
+import jakarta.mail.MessagingException;
 
 @Controller
 public class controller {
-	
+
 	@Autowired
 	private EmpService empService;
+	
+	@Autowired
+	private EmailService emailService;
+
 
 	@GetMapping("/")
 	public String index() {
 		return "index";
 	}
+
 	@GetMapping("/upload")
 	public String upload() {
 		return "upload";
 	}
-	
-	@GetMapping("/excel")
-	public ResponseEntity<Resource> download() throws IOException {
 
-		String filename = "empList.xlsx";
-
-		ByteArrayInputStream actualData = empService.getActualData();
-		InputStreamResource file = new InputStreamResource(actualData);
-
-		ResponseEntity<Resource> body = ResponseEntity.ok()
-				.header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename= " + filename)
-				.contentType(MediaType.parseMediaType("application/vnd.ms-excel")).body(file);
-
-		return body;
+	@GetMapping("/mail")
+	public String mail() {
+		return "sendMail";
 	}
-	
+
 	@PostMapping("/upload/file")
-	public ResponseEntity<?> upload(@RequestParam("file") MultipartFile file) {
-		if (Helper.checkExcelFormat(file)) {
-			// true
-
-			this.empService.save(file);
-
-			return ResponseEntity.ok(Map.of("message", "File is uploaded and data is saved to db"));
-
+	public ResponseEntity<String> uploadExcelFile(@RequestParam("file") MultipartFile file) {
+		try {
+			empService.saveDataFromExcelFile(file);
+			return ResponseEntity.ok("Data saved successfully");
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+					.body("Error occurred while saving data: " + e.getMessage());
 		}
-		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Please upload excel file ");
 	}
-	
+
+	@GetMapping("/emplist")
+	public String showEmpList(Model model) {
+		List<employe> emp = empService.getAllEmployees();
+		model.addAttribute("emp", emp);
+
+		return "showEmp";
+
+	}
+
+	@GetMapping("/approve/{id}")
+	public String approveEmp(@PathVariable int id) {
+		employe existingEmp = empService.getEmpById(id);
+		empService.updateEmp(id, "Approved");
+		return "redirect:/emplist";
+	}
+
+	@GetMapping("/reject/{id}")
+	public String rejectEmp(@PathVariable int id) {
+		employe existingEmp = empService.getEmpById(id);
+		empService.updateEmp(id, "Reject");
+		return "redirect:/emplist";
+	}
+
+	@PostMapping("/sendAppEmp")
+	public String sendApprovedEmployee(@RequestParam("email") String email) throws MessagingException {
+		
+		List<employe> approvedEmp=empService.getApprovedEmp();
+		
+		StringBuilder emailBody = new StringBuilder("<h1>Approved Employee</h1>");
+		emailBody.append("<table border='1'><tr><th>Id</th><th>Name</th><th>age</th><th>addr1</th><th>addr2</th><th>City</th><th>State</th><th>Country</th><th>Pincode</th><th>Project</th><th>ManagerId</th><th>Date</th><th>LoginTime</th><th>LogoutTime</th><th>Status</th></tr>");
+		
+		for(employe emp: approvedEmp) {
+			emailBody.append("<tr>")
+			.append("<td>").append(emp.getId()).append("</td>")
+			.append("<td>").append(emp.getName()).append("</td>")
+			.append("<td>").append(emp.getAge()).append("</td>")
+			.append("<td>").append(emp.getAddr1()).append("</td>")
+			.append("<td>").append(emp.getAddr2()).append("</td>")
+			.append("<td>").append(emp.getCity()).append("</td>")
+			.append("<td>").append(emp.getState()).append("</td>")
+			.append("<td>").append(emp.getCountry()).append("</td>")
+			.append("<td>").append(emp.getPincode()).append("</td>")
+			.append("<td>").append(emp.getProject()).append("</td>")
+			.append("<td>").append(emp.getManagerId()).append("</td>")
+			.append("<td>").append(emp.getDate()).append("</td>")
+			.append("<td>").append(emp.getLoginTime()).append("</td>")
+			.append("<td>").append(emp.getLogoutTime()).append("</td>")
+			.append("<td>").append(emp.getApproved()).append("</td>")
+			.append("/tr");
+			
+		}
+		emailBody.append("</table>");
+		
+		emailService.sendApprovedEmpEmail(email, "Approved Employees List", emailBody.toString());
+		
+		return "redirect:/emplist";
+		
+	}
 }
